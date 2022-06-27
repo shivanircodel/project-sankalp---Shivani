fde23e7bb24174ac53c4de60b1570efb16e01a73
Shivani Rajgopal <88949973+shivanircodel@users.noreply.github.com>
2022-06-26 20:42:45 -0400

    Create curl-test.sh

---
+++ app/curl-test.sh
@@ -0,0 +1,3 @@
+CURL --request POST http://localhost:5000/api/timeline_post -d 'name=Arpitha&email=arpitha.rao@mlh.io&content=JustAdded Database to my portfolio site!'
+
+CURL --request GET http://localhost:5000/api/timeline_post
