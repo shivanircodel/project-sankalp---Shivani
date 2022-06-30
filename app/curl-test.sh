d897a4f581b8ce9aeb858321f9b92c17fb5e12d8
Shivani Rajgopal <88949973+shivanircodel@users.noreply.github.com>
2022-06-30 15:02:21 -0400

    Update curl-test.sh

--- app/curl-test.sh
+++ app/curl-test.sh
@@ -1,3 +1,3 @@
-CURL --request POST http://localhost:5000/api/timeline_post -d 'name=Arpitha&email=arpitha.rao@mlh.io&content=JustAdded Database to my portfolio site!'
+CURL --request POST http://localhost:5000/api/timeline_post -d 'name=Shivani&email=shivanirajgopal@gmail.com&content=JustAdded Database to my portfolio site!'
 
 CURL --request GET http://localhost:5000/api/timeline_post
