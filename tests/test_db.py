6025f0c7d0e6474224bc6d509d99a05e390deaca
Aima Alakhume <71132341+AimaAlakhume@users.noreply.github.com>
2022-07-05 23:09:03 -0400

    Update test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,38 +1,28 @@
+# tests/test_app.py
+
 import unittest
-from peewee import *
+import os
+os.environ['TESTING'] = 'true'
 
-from app import TimelinePost
+from app import app
 
-MODELS = [TimelinePost]
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
 
-# use an in-memory SQLite for tests
-test_db = SqliteDatabase(':memory:')
+    def test_home(self):
+        response = self.client.get("/")
+        assert response.status_code == 200
+        html = response.get_data(as_text=True)
+        assert '<title>MLH Fellow</title>' in html
+        # TODO Add more tests relating to the home page
 
-class TestTimelinePost(unittest.TestCase):
-	def setUp(self):
-		# Bind model classes to test db. Since we have a complete list of all models, we do not 
-		# need recursively bind dependencies.
-		test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
-
-		test_db.connect()
-		test_db.create_tables(MODELS)
-	
-	def tearDown(self):
-		# Not strictly necessary since SQLite in-memory databases only live for the duration of
-		# the connection, and in the next step we close the connection..but a good practice all
-		# the same
-		test_db.drop_tables(MODELS)
-
-		# Close connection to db
-		test_db.close()
-
-	def test_timeline_post(self):
-		# Create 2 timeline posts
-		first_post = TimelinePost.create(name='John Doe', email='john@example.com', content='Hello world, I\'m John!')
-		assert first_post.id == 1
-    
-		second_post = TimelinePost.create(name='Jane Doe', email='jame@example.com', content='Hello world, I\'m Jane!')
-		assert second_post.id == 2
-		
-		response = get_timeline_posts()
-		assert len(response['timeline_posts']) == 2
+    def test_timeline(self):
+        response = self.client.get('/api/timeline_post')
+        assert response.status_code == 200
+        assert response.is_json
+        json = response.get_json()
+        assert 'timeline_posts' in json
+        assert len(json['timeline_posts']) == 0
+        # TODO Add more tests relating to the /api/timeline_post GET and POST apis
+        # TODO Add  more tests relating to the timeline page
