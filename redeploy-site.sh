4b5fda625a0393a8ef2c105ebbd97b581c18d37b
Shivani Rajgopal <88949973+shivanircodel@users.noreply.github.com>
2022-07-24 19:16:52 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-cd project-sankalp
+cd project-sankalp---Shivani
 
 git fetch
 git reset origin/main --hard
 
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
+
+docker-compose ps
